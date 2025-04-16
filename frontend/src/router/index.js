417a28997f94c39ea274c534c5b37df8ab4b1d14
lzhang121 <zhpsark@gmail.com>
2025-04-16 17:55:59 +0900

    結果ページを更新すると404エラーが発生する問題を解決します

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import UserPage from '../views/UserPage.vue';
 import PageResult from '../views/PageResult.vue';
 
@@ -17,7 +17,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 });
 
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   devServer: {
-    hot: false,              // 关闭热模块替换
-    liveReload: false,       // 关闭页面自动刷新
-    client: false,           // ✅ 彻底禁用 websocket 客户端注入
+    hot: false,
+    liveReload: false,
+    client: false,
+    historyApiFallback: true,
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
