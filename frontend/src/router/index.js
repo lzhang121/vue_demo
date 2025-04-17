7a833a6d9de35c19ccfeb0c5e07b5b22b4d9f003
张雷 <zhanglei@bonree.com>
2025-04-18 08:57:44 +0900

    reconstruct

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import FormPage from '../views/FormPage.vue';
+import SuccessPage from '../views/SuccessPage.vue';
+import FailurePage from '../views/FailurePage.vue';
+import LoadingPage from '../views/LoadingPage.vue';
 import DownloadPage from '../views/DownloadPage.vue';
 
 const routes = [
@@ -12,6 +15,24 @@ const routes = [
     path: '/download',
     name: 'DownloadPage',
     component: DownloadPage,
+    props: true  // 允许通过 props 传递参数
+  },
+  {
+    path: '/loading',
+    name: 'LoadingPage',
+    component: LoadingPage,
+    props: true  // 允许通过 props 传递参数
+  },
+  {
+    path: '/success',
+    name: 'SuccessPage',
+    component: SuccessPage,
+    props: true  // 允许通过 props 传递参数
+  },
+  {
+    path: '/failure',
+    name: 'FailurePage',
+    component: FailurePage,
     props: true  // 允许通过 props 传递参数
   }
 ];
---
+++ frontend/src/views/FailurePage.vue
@@ -0,0 +1,12 @@
+
+<template>
+    <div class="result">
+        <TopBar />
+        <CenteredMessage message="検出失敗" color="#1890ff" />
+    </div>
+ </template>
+
+<script setup>
+    import TopBar from '@/components/TopBararea.vue'
+    import CenteredMessage from '@/components/ReturnPage.vue'
+</script>
